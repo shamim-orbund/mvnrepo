2d5e1045aa8b4780158db6304facf7b9a7682635
Md Shah Alam Shamim <shamimewu.orbund@gmail.com>
2020-04-06 21:41:19 +0600

    Initial spring boot entity application is generated

---
+++ src/main/java/com/orbund/galactic/be/common/entities/GalacticBeCommonEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.orbund.galactic.be.common.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticBeCommonEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticBeCommonEntitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orbund/galactic/be/common/entities/GalacticBeCommonEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbund.galactic.be.common.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticBeCommonEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
